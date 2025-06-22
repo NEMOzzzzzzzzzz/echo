0aa36420ff8675d6c2773ec550ccc0c25d12c20b
ivarunnemo <unknowndude1923@gmail.com>
2025-06-22 16:59:05 +0530

    Removed Corrupted files

--- src/utils/uploadFile.js
+++
@@ -1,8 +0,0 @@
-import { ref, uploadBytes } from "firebase/storage";
-import { storage } from "../../firebase";
-
-export async function uploadFile(file) {
-  const fileRef = ref(storage, `uploads/${file.name}`);
-  const snapshot = await uploadBytes(fileRef, file);
-  return snapshot;
-}
